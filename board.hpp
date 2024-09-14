8a97b9a80aba925f38182c9085d340f68276b0f9
naphalm <master.sys.vp@gmail.com>
2024-09-14 16:22:10 +0300

    board.hpp

--- board.hpp
+++ board.hpp
@@ -3,7 +3,22 @@
 class Board {
     int width;
     int height;
+
 public:
-    Board(int w, int h);
-    void Draw() const;
+    Board(int w, int h); 
+    int GetWidth() const; 
+    int GetHeight() const; 
 };
+
+
+Board::Board(int w, int h) : width(w), height(h) {}
+
+
+int Board::GetWidth() const {
+    return width;
+}
+
+
+int Board::GetHeight() const {
+    return height;
+}
