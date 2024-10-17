8d1e6562a57ed181d251daaf182435e782b3b285
naphalm <master.sys.vp@gmail.com>
2024-10-17 21:38:07 +0300

    Point.hpp

--- Point.hpp
+++ Point.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <iostream>
+
 struct Point {
     int x;
     int y;
 
-    Point() : x(0), y(0) {}  
-    Point(int xPos, int yPos) : x(xPos), y(yPos) {}  
-};
+    Point(int _x = 0, int _y = 0);        
+    Point(const Point& other);            
 
+    Point operator=(const Point& other);  
+    bool operator==(const Point& other) const;  
+
+    friend std::istream& operator>>(std::istream& in, Point& point);  
+    friend std::ostream& operator<<(std::ostream& out, const Point& point);  
+};
 
