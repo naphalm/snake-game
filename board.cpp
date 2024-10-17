dad1ceead949388870916d581aa203aea712da19
naphalm <master.sys.vp@gmail.com>
2024-10-17 21:39:45 +0300

    board.cpp

--- board.cpp
+++ board.cpp
@@ -1,14 +1,35 @@
 #include "board.hpp"
 
+Board::Board(int width, int height) : _width(width), _height(height) {}
 
-Board::Board(int w, int h) : width(w), height(h) {}
-
+Board::Board(const Board& other) : _width(other.GetWidth()), _height(other.GetHeight()) {}
 
 int Board::GetWidth() const {
-    return width;
+    return _width;
 }
 
-
 int Board::GetHeight() const {
-    return height;
+    return _height;
+}
+
+Board Board::operator=(const Board& other) {
+    _width = other.GetWidth();
+    _height = other.GetHeight();
+    return *this;
+}
+
+bool Board::operator==(const Board& other) const {
+    return GetWidth() == other.GetWidth() && GetHeight() == other.GetHeight();
+}
+
+std::istream& operator>>(std::istream& in, Board& board) {
+    int width, height;
+    in >> width >> height;
+    board = Board(width, height);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Board& board) {
+    out << board.GetWidth() << " " << board.GetHeight();
+    return out;
 }
