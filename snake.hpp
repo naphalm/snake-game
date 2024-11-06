d190a18d058c7a77ab4f29268c47f10ccec1dbd6
naphalm <master.sys.vp@gmail.com>
2024-11-06 20:15:01 +0200

    snake.hpp

--- snake.hpp
+++ snake.hpp
@@ -1,43 +1,15 @@
 #pragma once
 #include "point.hpp"
+#include <vector>
+#include <memory>
 
 class Snake {
-    Point segments[100];  
-    int length;           
+    std::vector<Point> segments; // Folosim vector pentru a stoca segmentele
+    int length;
 
 public:
     Snake();              
-    void Move(Point direction); 
+    void Move(const Point& direction); 
     void Grow();         
     Point GetHeadPosition() const; 
 };
-
-
-Snake::Snake() {
-    length = 1;  
-    segments[0] = Point(10, 10);  
-}
-
-
-void Snake::Move(Point direction) {
-   
-    for (int i = length - 1; i > 0; --i) {
-        segments[i] = segments[i - 1];
-    }
-
-    segments[0].x += direction.x;
-    segments[0].y += direction.y;
-}
-
-
-void Snake::Grow() {
-    if (length < 100) {
-        segments[length] = segments[length - 1]; 
-        length++;
-    }
-}
-
-
-Point Snake::GetHeadPosition() const {
-    return segments[0];
-}
