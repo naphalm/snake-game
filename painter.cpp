d34bf6e088c4f0761f619d23eb34a02d822edcfb
naphalm <master.sys.vp@gmail.com>
2024-11-06 20:17:27 +0200

    painter.cpp

--- painter.cpp
+++ painter.cpp
@@ -1,13 +1,11 @@
 #include "painter.hpp"
 #include <iostream>
 
-void Painter::DrawImage(Point topLeft, Point bottomRight, char** image) {
+void Painter::DrawImage(Point topLeft, Point bottomRight, std::vector<std::string>& image) {
     std::cout << "Drawing image from (" << topLeft.x << ", " << topLeft.y << ") to ("
               << bottomRight.x << ", " << bottomRight.y << ")" << std::endl;
-    
 }
 
-void Painter::WriteText(Point position, char* text) {
+void Painter::WriteText(Point position, const std::string& text) {
     std::cout << "Writing text at (" << position.x << ", " << position.y << "): " << text << std::endl;
-   
 }
