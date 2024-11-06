6194085c6344eaecd2390d6e9113b95700565610
naphalm <master.sys.vp@gmail.com>
2024-11-06 20:15:50 +0200

    snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,31 +1,27 @@
 #include "snake.hpp"
 
-
-Snake::Snake() {
-    segments[0] = Point(10, 10); 
-    length = 1;
+Snake::Snake() : length(1) {
+    segments.push_back(Point(10, 10)); 
 }
 
+void Snake::Move(const Point& direction) {
+    if (segments.empty()) return;
 
-void Snake::Move(Point direction) {
-   
+ 
     for (int i = length - 1; i > 0; --i) {
-        segments[i] = segments[i - 1]; 
+        segments[i] = segments[i - 1];
     }
-    
-  
+
     segments[0].x += direction.x;
     segments[0].y += direction.y;
 }
 
-
 void Snake::Grow() {
     if (length < 100) {
-        segments[length] = segments[length - 1]; 
+        segments.push_back(segments[length - 1]); 
         length++;
     }
 }
-
 
 Point Snake::GetHeadPosition() const {
     return segments[0];
