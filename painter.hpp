ffed9adcebd2b835253cc920c41081e7204350aa
naphalm <master.sys.vp@gmail.com>
2024-11-06 20:17:53 +0200

    painter.hpp

--- painter.hpp
+++ painter.hpp
@@ -1,9 +1,10 @@
 #pragma once
-
 #include "abstract_painter.hpp"
+#include <string>
+#include <vector>
 
 class Painter : public AbstractPainter {
 public:
-    virtual void DrawImage(Point topLeft, Point bottomRight, char** image) override; 
-    virtual void WriteText(Point position, char* text) override;                     
+    virtual void DrawImage(Point topLeft, Point bottomRight, std::vector<std::string>& image) override; 
+    virtual void WriteText(Point position, const std::string& text) override;                     
 };
