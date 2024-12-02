ff30614653948a82ef1bce1ba3e30a082648a288
naphalm <master.sys.vp@gmail.com>
2024-12-02 13:44:34 +0200

    snake.hpp

--- snake.hpp
+++ snake.hpp
@@ -1,15 +1,43 @@
 #pragma once
 #include "point.hpp"
 #include <vector>
-#include <memory>
 
+/**
+ * @file snake.hpp
+ * @brief Declarația clasei Snake, ce reprezintă șarpele jocului.
+ * 
+ * @project Snake Game
+ * @date 2024
+ */
+
+/**
+ * @class Snake
+ * @brief Clasa ce modelează comportamentul șarpelui.
+ */
 class Snake {
-    std::vector<Point> segments; 
-    int length;
+    std::vector<Point> segments; ///< Segmentele șarpelui (cap + corp).
+    int length; ///< Lungimea curentă a șarpelui.
 
 public:
-    Snake();              
-    void Move(const Point& direction); 
-    void Grow();         
-    Point GetHeadPosition() const; 
+    /**
+     * @brief Constructor implicit. Inițializează șarpele.
+     */
+    Snake();
+
+    /**
+     * @brief Mișcă șarpele într-o anumită direcție.
+     * @param direction Direcția de mișcare.
+     */
+    void Move(const Point& direction);
+
+    /**
+     * @brief Crește lungimea șarpelui.
+     */
+    void Grow();
+
+    /**
+     * @brief Obține poziția capului șarpelui.
+     * @return Poziția punctului cap.
+     */
+    Point GetHeadPosition() const;
 };
